026e3b6db90a9955926a6607abeff38f33113072
Stefan Kanev <stefan.kanev@gmail.com>
2012-07-09 22:15:27 +0300

    Add simple_form

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.1.1'
 
 gem 'pg'
+gem 'simple_form'
 
 gem 'jquery-rails'
 
---
+++ config/initializers/simple_form.rb
@@ -0,0 +1,23 @@
+SimpleForm.setup do |config|
+  config.wrappers :default, class: :input, hint_class: :field_with_hint, error_class: :field_with_errors do |b|
+    b.use :html5
+
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+
+    b.use :label_input
+    b.use :hint,  wrap_with: {tag: :span, class: :hint }
+    b.use :error, wrap_with: {tag: :span, class: :error }
+  end
+
+  config.default_wrapper = :default
+
+  config.boolean_style = :inline
+  config.error_notification_tag = :div
+  config.error_notification_class = 'alert alert-error'
+  config.required_by_default = false
+  config.browser_validations = false
+end
