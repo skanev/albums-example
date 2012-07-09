0a51b71ef3d66664bc53a0358e9a1a0f961af360
Stefan Kanev <stefan.kanev@gmail.com>
2012-07-09 23:18:52 +0300

    Move a stepdef to its rightful place
    How did I even let that happen?

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -2,6 +2,11 @@ Given 'a user "$email" with password "$password"' do |email, password|
   create :user, email: email, password: password
 end
 
+Given 'I am logged in' do
+  user = create :user
+  visit backdoor_login_path(email: user.email)
+end
+
 When 'I attempt to log in with email "$email" and password "$password"' do |email, password|
   visit new_user_session_path
 
